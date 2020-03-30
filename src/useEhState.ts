163006381f8b7bd327af9e4c7a5932c4bde846ae
Oran <orangilboa@gmail.com>
2020-03-31 01:41:10 +0300

    bug fix: state hook held reference to state object, caused no rerender on update

--- src/useEhState.ts
+++ src/useEhState.ts
@@ -1,8 +1,8 @@
 import eh, { EhState, EventHub } from '@foursk/eh';
 import { useEh } from './useEh';
 
-const dataToProps = <T>(data: T) => data;
+const dataToProps = <T>(data: T) => Object.assign({}, data);
 
 export function useEhState<T_EVENTDATA extends object>(ehState: EhState<T_EVENTDATA>, eventHub: EventHub = eh) {
-  return useEh<T_EVENTDATA, T_EVENTDATA>(ehState.name, ehState.state, dataToProps, eventHub);
+  return useEh<T_EVENTDATA, T_EVENTDATA>(ehState.name, Object.assign({}, ehState.state), dataToProps, eventHub);
 }
