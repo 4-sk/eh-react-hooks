7f6e77c9c70657778d057f6a23b3d44c4401e03d
Oran <orangilboa@gmail.com>
2020-03-31 00:44:46 +0300

    added use ehState

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { useEh } from './useEh';
 import { useEhEvent } from './useEhEvent';
+import { useEhState } from './useEhState';
 
-export { useEh, useEhEvent };
+export { useEh, useEhEvent, useEhState };
--- src/useEhEvent.ts
+++ src/useEhEvent.ts
@@ -2,11 +2,11 @@ import eh, { EhEvent, EventHub } from '@foursk/eh';
 import { EventToProps } from './common';
 import { useEh } from './useEh';
 
-export function useEhEvent<T_EVENTDATA, T_PROPS = T_EVENTDATA>
-  (
-    ehEvent: EhEvent<T_EVENTDATA>,
-    defaultProps: T_PROPS,
-    eventToProps: EventToProps<T_EVENTDATA, T_PROPS> = data => (data as unknown) as T_PROPS,
-    eventHub: EventHub = eh) {
+export function useEhEvent<T_EVENTDATA extends object, T_PROPS = T_EVENTDATA>(
+  ehEvent: EhEvent<T_EVENTDATA>,
+  defaultProps: T_PROPS,
+  eventToProps: EventToProps<T_EVENTDATA, T_PROPS> = data => (data as unknown) as T_PROPS,
+  eventHub: EventHub = eh,
+) {
   return useEh(ehEvent.name, defaultProps, eventToProps, eventHub);
 }
