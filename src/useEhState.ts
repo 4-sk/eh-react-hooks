4ff00176beac2bbcac9bdc29906f1cb60012e586
Oran <orangilboa@gmail.com>
2020-03-31 00:53:13 +0300

    fix import order

--- src/useEhState.ts
+++ src/useEhState.ts
@@ -1,12 +1,8 @@
-import eh, { EventHub, EhState } from '@foursk/eh';
-import { EventToProps } from './common';
+import eh, { EhState, EventHub } from '@foursk/eh';
 import { useEh } from './useEh';
 
-const dataToProps = <T>(data:T) => data;
+const dataToProps = <T>(data: T) => data;
 
-export function useEhState<T_EVENTDATA extends object>(
-  ehState: EhState<T_EVENTDATA>,
-  eventHub: EventHub = eh,
-) {
+export function useEhState<T_EVENTDATA extends object>(ehState: EhState<T_EVENTDATA>, eventHub: EventHub = eh) {
   return useEh<T_EVENTDATA, T_EVENTDATA>(ehState.name, ehState.state, dataToProps, eventHub);
 }
